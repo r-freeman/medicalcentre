52d6004d64e6920e90a433b5d405b3748d156a58
Ryan Freeman <info@ryanfreeman.io>
2019-10-28 17:34:06 +0000

    Added alter_users_table migration to add address and phone columns

---
+++ database/migrations/2019_10_28_172331_alter_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // add address column after name
+            $table->string('address')->after('name');
+
+            // add phone column after address
+            $table->string('phone')->after('address');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // drop address and phone columns
+            $table->dropColumn([
+                'address',
+                'phone'
+            ]);
+        });
+    }
+}
