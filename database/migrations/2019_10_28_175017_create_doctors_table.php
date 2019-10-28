3f54086bb4fbe9af529551d3da27726a493098df
Ryan Freeman <info@ryanfreeman.io>
2019-10-28 17:57:14 +0000

    Added create_doctors_table migration

---
+++ database/migrations/2019_10_28_175017_create_doctors_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDoctorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('start_date');
+            $table->bigInteger('user_id')->unsigned();
+            $table->timestamps();
+
+            // foreign key constraint on doctors.user_id
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors');
+    }
+}
