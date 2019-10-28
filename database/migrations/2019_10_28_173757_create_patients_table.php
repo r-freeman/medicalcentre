52814a6440c42feb65f84a2fcd681f16b7bf1971
Ryan Freeman <info@ryanfreeman.io>
2019-10-28 17:56:43 +0000

    Added create_patients_table migration

---
+++ database/migrations/2019_10_28_173757_create_patients_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->tinyInteger('insured');
+            $table->string('policy_no');
+            $table->bigInteger('user_id')->unsigned();
+            $table->timestamps();
+
+            // foreign key constraint on patients.user_id
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
