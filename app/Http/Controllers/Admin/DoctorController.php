ad8a8fe13ec660a9b8f492d76876e04751bf9ca0
Ryan Freeman <info@ryanfreeman.io>
2019-11-11 20:03:10 +0000

    Added logic for index method on Admin/DoctorController and added admin doctors index blade template

---
+++ app/Http/Controllers/Admin/DoctorController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+
+class DoctorController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('role:admin');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // return collection of users with doctor role
+        $doctors = User::whereHas('roles', function ($q) {
+            $q->where('name', 'doctor');
+        })->get();
+
+        foreach ($doctors as $doctor) {
+            // add attributes from the role_data pivot table to each doctor instance
+            $doctor->addAttributesFromPivot('doctor', ['start_date']);
+        }
+
+        return view('admin.doctors.index')
+            ->with([
+                'doctors' => $doctors
+            ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/admin/doctors/index.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="d-flex justify-content-between">
+                            <p class="mb-0">Doctors</p>
+                            <a href="{{ route('admin.doctors.create') }}">Add New Doctor</a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        @if (count($doctors) === 0)
+                            <p>There are no doctors.</p>
+                        @else
+                            <table class="table table-hover" id="doctors-table">
+                                <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Phone</th>
+                                    <th>Email</th>
+                                    <th></th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($doctors as $doctor)
+                                    <tr data-id="{{ $doctor->id }}">
+                                        <td> {{ $doctor->name }}</td>
+                                        <td> {{ $doctor->phone }}</td>
+                                        <td> {{ $doctor->email }}</td>
+                                        <td>
+                                            <div class="btn-group-md" role="group" aria-label="Basic example">
+                                                <a href="{{ route('admin.doctors.show', $doctor->id) }}"
+                                                   class="btn btn-outline-success">View</a>
+                                                <a href="{{ route('admin.doctors.edit', $doctor->id) }}"
+                                                   class="btn btn-outline-primary">Edit</a>
+                                                <form action="{{ route('admin.doctors.destroy', $doctor->id) }}"
+                                                      style="display: inline-block" method="POST">
+                                                    <input type="hidden" name="_method" value="DELETE">
+                                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                                    <button type="submit" class="form-control btn btn-outline-danger"
+                                                            onclick="return confirm('Delete {{ $doctor->name }}?')">
+                                                        Delete
+                                                    </button>
+                                                </form>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
