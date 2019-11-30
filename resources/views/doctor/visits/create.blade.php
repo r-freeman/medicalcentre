8f66ced23d3bf69301c7ed6c77996fc0aacf8e6c
Ryan Freeman <info@ryanfreeman.io>
2019-11-30 17:25:31 +0000

    Added template for doctor to create visit

---
+++ resources/views/doctor/visits/create.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="card">
+                    <div class="card-header">Add new visit</div>
+                    <div class="card-body">
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <form action="{{ route('doctor.visits.store') }}" method="POST">
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            <div class="form-group">
+                                <label for="date">Date</label>
+                                <input type="date" class="form-control" id="date" name="date" value="{{ old('date') }}">
+                            </div>
+                            <div class="form-group">
+                                <label for="time">Time</label>
+                                <input type="time" class="form-control" id="time" name="time" min="09:00" max="17:00" value="{{ old('time') }}">
+                            </div>
+                            <div class="form-group">
+                                <label for="duration">Duration</label>
+                                <input type="text" class="form-control" id="duration" name="duration"
+                                       value="{{ old('duration') }}" placeholder="Minutes">
+                            </div>
+                            <div class="form-group">
+                                <label for="patient_id">Patient</label>
+                                <select class="form-control" id="patient_id" name="patient_id">
+                                    @foreach($patients as $patient)
+                                        <option value="{{ $patient->id }}">{{ $patient->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <input type="hidden" id="doctor_id" name="doctor_id" value="{{ Auth::user()->id }}">
+                            <div class="form-group">
+                                <label for="cost">Cost</label>
+                                <input type="text" class="form-control" id="cost" name="cost"
+                                       value="{{ old('cost') }}">
+                            </div>
+                            <button class="btn btn-outline-success" type="submit">Submit</button>
+                            <a href="{{ route('home') }}" onclick="window.history.go(-1); return false;" class="btn btn-link float-right">Cancel</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
